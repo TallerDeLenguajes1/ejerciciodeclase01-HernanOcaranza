9b91fa78dc961ded0cd00c999d0e2fa0647da227
Hernan Ocaranza <hernanocaranza@hotmail.com.ar>
2021-08-18 17:56:01 -0300

    Se agrega el proyecto de consola AdivinarElNumero

---
+++ AdivinarElNumero/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AdivinarElNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var num = r.Next(101);
+            int res;
+            int intentos = 0;
+            do
+            {
+                Console.WriteLine("Elija el numero: ");
+                res = Convert.ToInt32(Console.ReadLine());
+                if (res > num)
+                    Console.WriteLine($"El numero {res} es mayor que el numero aleatorio\n");
+                if (res < num)
+                    Console.WriteLine($"El numero {res} es menor que el numero aleatorio\n");
+
+                intentos++;
+            } while (num != res);
+
+            Console.WriteLine($"Encontraste el numero {num} en {intentos} intentos");
+        }
+    }
+}
